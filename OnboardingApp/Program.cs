d35266b211f4a8359a4dd1c47721ad3420fc72e9
Starla Oden <starlaoh@gmail.com>
2018-01-27 13:03:10 -0800

    Added First and Last Name

--- OnboardingApp/Program.cs
+++ OnboardingApp/Program.cs
@@ -10,6 +10,15 @@ namespace OnboardingApp
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Hello, and welcome to our OnBoarding Application");
+
+            Console.WriteLine("What is your first name?");
+            var firstName = Console.ReadLine();
+            Console.WriteLine($"Great! Hi { firstName}!");
+
+            Console.WriteLine("What is your last name?");
+            var lastName = Console.ReadLine();
+            Console.WriteLine($"Great! Hi {firstName}{lastName}");
         }
     }
 }
